0db42a74a989e8d747565fcfebee8ada2a3a4f11
inti0234 <inti0234@INTI-0234>
2016-09-22 16:40:06 +0200

    yo oliv recup moi ca

--- Projet_git/src/Test.java
+++ Projet_git/src/Test.java
@@ -1,4 +1,53 @@
 
 public class Test {
 
+	
+	private Long id;
+	private String nom;
+	private String prenom;
+	
+	
+	public Test(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+
+
+	public Test() {
+		super();
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+	
 }
